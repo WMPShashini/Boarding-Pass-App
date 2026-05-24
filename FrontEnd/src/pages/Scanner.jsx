c2e8c50eaab4b4b35972e0c977097431160c5781
WMPShashini <shashiniwmp.22@uom.lk>
2026-05-24 17:22:22 +0530

    fix netlify link

--- FrontEnd/src/pages/Scanner.jsx
+++ FrontEnd/src/pages/Scanner.jsx
@@ -3,7 +3,7 @@ import { QRCodeCanvas } from "qrcode.react";
 const Scanner = () => {
 
   // IMPORTANT: must be reachable from PHONE
-  const qrValue = "https://lighthearted-khapse-dcbfcc.netlify.app/scan";
+  const qrValue = "https://phenomenal-klepon-ba6c29.netlify.app/scan";
 
   return (
     <div style={{ textAlign: "center", marginTop: "60px" }}>
