5d0f5f39f8305ef2c4b7fbe4054487258c86271d
WMPShashini <shashiniwmp.22@uom.lk>
2026-05-24 16:56:34 +0530

    fix routing

--- FrontEnd/src/pages/Scanner.jsx
+++ FrontEnd/src/pages/Scanner.jsx
@@ -3,7 +3,7 @@ import { QRCodeCanvas } from "qrcode.react";
 const Scanner = () => {
 
   // IMPORTANT: must be reachable from PHONE
-  const qrValue = "https://6a12d4b972ba470008e3aa0a--stellar-crumble-23f63a.netlify.app/scan";
+  const qrValue = "https://lighthearted-khapse-dcbfcc.netlify.app/scan";
 
   return (
     <div style={{ textAlign: "center", marginTop: "60px" }}>
